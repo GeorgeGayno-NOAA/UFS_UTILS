82cbee98db97169cc1ccbd5fcce38b9e536bcbdc
George Gayno <George.Gayno@noaa.gov>
2020-12-22 14:20:28 +0000

    gh-pages  updates

--- search/all_72.js
+++ search/all_72.js
@@ -7,6 +7,7 @@ var searchData=
   ['read_5fmosaic_2ec',['read_mosaic.c',['../read__mosaic_8c.html',1,'']]],
   ['read_5fmosaic_2eh',['read_mosaic.h',['../read__mosaic_8h.html',1,'']]],
   ['read_5fsetup_5fnamelist',['read_setup_namelist',['../classprogram__setup.html#a59431f09ff79583ed8dcea7de2fa918d',1,'program_setup::read_setup_namelist'],['../classprogram__setup.html#ac52f11da9f12e0cf9ff87fae136f0cde',1,'program_setup::read_setup_namelist(localpet)']]],
+  ['read_5fstatic_5ffile',['read_static_file',['../classstatic__data.html#aff2a5a5fa372950e606431e9ee777e65',1,'static_data']]],
   ['read_5ftfs_5fnc',['read_tfs_nc',['../read__tfs__nc_8f90.html#a8c9a60b111795f2990e7e0c561887c0c',1,'read_tfs_nc.f90']]],
   ['read_5ftfs_5fnc_2ef90',['read_tfs_nc.f90',['../read__tfs__nc_8f90.html',1,'']]],
   ['read_5ftfs_5fnc_5f2d_2ef90',['read_tfs_nc_2d.f90',['../read__tfs__nc__2d_8f90.html',1,'']]],
